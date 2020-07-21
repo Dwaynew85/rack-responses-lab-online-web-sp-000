ccd573f0b25b5472f163a2cae972cb76f85cd8f3
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-21 07:39:41 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,10 @@
+class Application 
+
+  def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, greeting]
+  end
+
+  def greeting
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+  end 
+end
